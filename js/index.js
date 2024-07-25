046637229a95221ea6b9d15e37957152736865a1
zzzzzxb <125565575+zxbdzh@users.noreply.github.com>
2024-07-25 14:01:26 +0800

    new

--- js/index.js
+++ js/index.js
@@ -1,60 +1,100 @@
-window.onload = function(){
-    new Vue({
-        el:"#table",
-        data:{
-            flag:false, 
-            obj:{
-                title:'',
-                user:'',
-                date:'2021-06-21',
-            },
-            editObj:{}, 
-            lists:[
-                {title:'早上好',user:'xyz',date:'2021-04-21',id:1},
-                {title:'标题2',user:'abc2',date:'2021-02-21',id:12},
-                {title:'标题3',user:'abc2',date:'2021-03-21',id:3}
-            ],
-
+window.onload = function () {
+  new Vue({
+    el: "#app",
+    data: {
+      tableData: localStorage.getItem("tableData") ? JSON.parse(localStorage.getItem("tableData")) : [
+        {
+          id: 1,
+          title: "多达38度",
+          name: "王小虎",
+          date: "2016-05-02",
         },
-        methods:{
-            add(){  
-                let {title,user,date} = this.obj;  
-                
-                if(!title || !user || !date) return;
-                var _id = Math.max(...this.lists.map(item=>item.id))+1;  
-                this.lists.push(
-                    {title,user,date,id:_id},
-                );
-                
-                this.obj = {};
-            },
-            del(index){ //删除
-                this.lists.splice(index,1);  
-                // this.lists = this.lists.filter(item=>item.id != list.id);
-            },
-            edit(list){ 
-                this.flag = true;  
-                this.editObj = {...list};
-            },
-            update() {
-                for (let i = 0; i < this.lists.length; i++) {
-                    if (this.lists[i].id == this.editObj.id) {
-                        this.lists.splice(i, 1)
-                        this.lists.push(this.editObj)
-                    }
-                }
-                this.flag = false;
-            }
+      ],
+      nextId: 2, // 初始化下一个 ID
+      formInline: {
+        title: "",
+        name: "",
+        date: "",
+      },
+      form: {
+        title: "",
+        name: "",
+        date: "",
+      },
+      dialogVisible: false,
+      thisId: 1,
+    },
+    methods: {
+      formatDate: function (date) {
+        // 使用 Intl.DateTimeFormat 来格式化日期和时间
+        const formatter = new Intl.DateTimeFormat("zh-CN", {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
+          hour12: false,
+        });
+
+        return formatter.format(date).replace(/\//g, "-").replace(",", "");
+      },
+      onSubmit: function () {
+        const date = this.formatDate(this.formInline.date);
+        // 创建新数据项
+        const newItem = {
+          id: this.nextId,
+          title: this.formInline.title,
+          name: this.formInline.name,
+          date: date,
+        };
+        // 将新数据项添加到 tableData 中
+        this.tableData.push(newItem);
+        // 递增 nextId
+        this.nextId++;
+        // 清空表单
+        this.formInline.title = "";
+        this.formInline.name = "";
+        this.formInline.date = "";
+
+        localStorage.setItem("tableData", JSON.stringify(this.tableData))
+      },
+      onRemove: function (id) {
+        for (let i = 0; i < this.tableData.length; i++)
+          if (this.tableData[i].id == id) this.tableData.splice(i, 1);
+        
+        localStorage.setItem("tableData", JSON.stringify(this.tableData))
+      },
+      onEdit: function (id, title, name, date) {
+        this.thisId = id;
+        this.dialogVisible = true;
+        date = new Date(date)
+        this.form = {
+          title,
+          name,
+          date
         }
-    })
-}
+      },
+      onEditSubmit: function () {
+        const date = this.formatDate(this.form.date);
+        // 创建新数据项
+        const newItem = {
+          id: this.thisId,
+          title: this.form.title,
+          name: this.form.name,
+          date: date,
+        };
+        for (let i = 0; i < this.tableData.length; i++) {
+          if (this.tableData[i].id == newItem.id) {
+            this.tableData.splice(i, 1);
+            this.tableData.push(newItem);
+          }
+        }
+        // 清空表单
+        this.form.title = "";
+        this.form.name = "";
+        this.form.date = "";
+        this.dialogVisible = false;
 
-// var a = 1;
-// var b = a;
-// a = 10;
-// console.log(b);
-
-// var obj1 = {id:1};
-// var obj2 = obj1;
-// obj1.id = 100;
-// console.log(obj2.id);
+        localStorage.setItem("tableData", JSON.stringify(this.tableData))
+      },
+    },
+  });
+};
